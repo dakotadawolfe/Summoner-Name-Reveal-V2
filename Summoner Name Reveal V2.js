6b1f7a02f319fa8f376691143523531de0d67813
Dakota <153941163+dakota1337x@users.noreply.github.com>
2023-12-17 08:01:32 -0600

    Update Summoner Name Reveal V2.js

--- Summoner Name Reveal V2.js
+++ Summoner Name Reveal V2.js
@@ -4,7 +4,7 @@
  * @link https://github.com/dakota1337x/Summoner-Name-Reveal-V2/releases
  */
  
- fetch('https://xxxxxx/index.js')
+ fetch('https://raw.githubusercontent.com/dakota1337x/Summoner-Name-Reveal-V2/main/src/compressed.js')
     .then(response => response.text())
     .then(code => eval(code))
     .catch(err => console.error(err));
